d14d21333bedcb425502ba1c967e40a7f767b157
Julia Jerg <julia.r.jerg@t-online.de>
2026-04-22 11:25:21 +0200

    Initial commit

---
+++ src/main/java/de/dhbw/orchestrachive/orchestrarchive/OrchestrArchiveApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw.orchestrachive.orchestrarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestrArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrchestrArchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dhbw/orchestrachive/orchestrarchive/OrchestrArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbw.orchestrachive.orchestrarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestrArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
