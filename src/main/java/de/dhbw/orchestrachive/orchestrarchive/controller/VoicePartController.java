89c8d57fd251ea8c2224d18e0e3b387e4c1371f8
Julia Jerg <julia.r.jerg@t-online.de>
2026-05-21 23:01:45 +0200

    Add Category model and application properties

---
+++ src/main/java/de/dhbw/orchestrachive/orchestrarchive/controller/VoicePartController.java
@@ -0,0 +1,4 @@
+package de.dhbw.orchestrachive.orchestrarchive.controller;
+
+public class VoicePartController {
+}
---
+++ src/main/java/de/dhbw/orchestrachive/orchestrarchive/model/Category.java
@@ -0,0 +1,28 @@
+package de.dhbw.orchestrachive.orchestrarchive.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+    private String description;
+
+    public Category() {
+
+    }
+}
