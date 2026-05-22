7dcb13f18f9f315f72b529b18be7b2ea77aab920
Julia Jerg <julia.r.jerg@t-online.de>
2026-05-22 13:47:38 +0200

    Add VoicePart model

---
+++ src/main/java/de/dhbw/orchestrachive/orchestrarchive/model/VoicePart.java
@@ -0,0 +1,30 @@
+package de.dhbw.orchestrachive.orchestrarchive.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class VoicePart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String instrument;
+    private int partNumber;
+    private String instrumentGroup;
+    private int sheetCount;
+
+    @ManyToOne
+    @JoinColumn(name = "sheet_music_id")
+    private SheetMusic sheetMusic;
+
+    public VoicePart() {
+
+    }
+}
