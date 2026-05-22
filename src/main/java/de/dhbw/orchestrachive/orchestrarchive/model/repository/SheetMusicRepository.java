2a57151face58c602bf5cd0c2520ef2ce68cf026
Julia Jerg <julia.r.jerg@t-online.de>
2026-05-22 17:55:12 +0200

    Add CategoryRepository, SheetMusicRepository and VoicePartRepository

---
+++ src/main/java/de/dhbw/orchestrachive/orchestrarchive/model/repository/SheetMusicRepository.java
@@ -0,0 +1,8 @@
+package de.dhbw.orchestrachive.orchestrarchive.model.repository;
+
+import de.dhbw.orchestrachive.orchestrarchive.model.SheetMusic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SheetMusicRepository extends JpaRepository<SheetMusic, Long> {
+
+}
---
+++ src/main/java/de/dhbw/orchestrachive/orchestrarchive/model/repository/VoicePartRepository.java
@@ -0,0 +1,8 @@
+package de.dhbw.orchestrachive.orchestrarchive.model.repository;
+
+import de.dhbw.orchestrachive.orchestrarchive.model.VoicePart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoicePartRepository extends JpaRepository<VoicePart, Long> {
+
+}
