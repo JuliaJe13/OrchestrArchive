30d8fd732ee2fd8ad1ad3d136a098f654d2ca764
Julia Jerg <julia.r.jerg@t-online.de>
2026-05-22 18:00:54 +0200

    Add Changes: SheetMusicRepository and VoicePartRepository

--- src/main/java/de/dhbw/orchestrachive/orchestrarchive/model/repository/SheetMusicRepository.java
+++ src/main/java/de/dhbw/orchestrachive/orchestrarchive/model/repository/SheetMusicRepository.java
@@ -3,6 +3,10 @@ package de.dhbw.orchestrachive.orchestrarchive.model.repository;
 import de.dhbw.orchestrachive.orchestrarchive.model.SheetMusic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SheetMusicRepository extends JpaRepository<SheetMusic, Long> {
 
+    List<SheetMusic> findByTitleContainingIgnoreCase(String title);
+    List<SheetMusic> findByComposerContainingIgnoreCase(String composer);
 }
--- src/main/java/de/dhbw/orchestrachive/orchestrarchive/model/repository/VoicePartRepository.java
+++ src/main/java/de/dhbw/orchestrachive/orchestrarchive/model/repository/VoicePartRepository.java
@@ -3,6 +3,9 @@ package de.dhbw.orchestrachive.orchestrarchive.model.repository;
 import de.dhbw.orchestrachive.orchestrarchive.model.VoicePart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VoicePartRepository extends JpaRepository<VoicePart, Long> {
 
+    List<VoicePart> findByInstrumentContainingIgnoreCase(String instrument);
 }
